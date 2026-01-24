a424637a6d854480e24f5b4dadac0f7cf7082f3f
Ubuntu <ubuntu@ip-172-31-4-49.eu-west-1.compute.internal>
2026-01-24 15:28:26 +0000

    added file script

---
+++ hello_chatur.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo $SHELL
+
+echo "hello this is great starting in shell script"
+echo "this is a line2"
+echo "great"
+echo "success come with consistency"
+
+
+mkdir rancho
+echo "file created"
+touch second_file.txt
+
